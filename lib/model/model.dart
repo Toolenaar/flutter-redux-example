b9e01b1ee6203ea460ac4145a085ae48effe0b03
jochem toolenaar <jochemtoolenaar@MacBook-Pro-van-jochem.local>
2019-03-02 09:36:06 +0100

    part 1 completed

---
+++ lib/model/model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+
+class Item {
+  final int id;
+  final String body;
+  Item({@required this.id, @required this.body});
+
+  Item copyWith({int id, String body}) {
+    return Item(body: body ?? this.body, id: id ?? this.id);
+  }
+}
+
+class AppState {
+  final List<Item> items;
+
+  AppState({
+    @required this.items
+  });
+
+  AppState.initialState() : items = List.unmodifiable(<Item>[]);
+
+}
